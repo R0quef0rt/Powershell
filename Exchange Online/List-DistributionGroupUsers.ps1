7a5daf0b6e7e1d6254f598aade504ea95c765cac
R0quef0rt <ryanjbrooks11@gmail.com>
2017-05-30 19:36:08 -0500

    Add List-DistributionGroupUsers.ps1

---
+++ Exchange Online/List-DistributionGroupUsers.ps1
@@ -0,0 +1,20 @@
+﻿## Set servers which this script can connect to
+$exchangeserver = "server.domain.com"
+
+## Credentials for Azure
+Write-Host "Which credentials will you use for Exchange?" -ForegroundColor Red
+$credential = Get-Credential
+
+## Do Exchange Online stuff
+Write-Host "Connecting to Exchange Online..." -ForegroundColor Red
+$ExchangeSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $credential -Authentication Basic -AllowRedirection
+
+Import-PSSession $ExchangeSession
+
+$DistroName = Read-Host 'Enter Dynamic Distribution Group Name'
+$FTE = Get-DynamicDistributionGroup "$DistroName"
+Get-Recipient -RecipientPreviewFilter $FTE.RecipientFilter | fl Identity
+Remove-PSSession $ExchangeSession
+
+Read-Host "Press any key to exit..."
+exit
