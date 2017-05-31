f2d20e25930d05d175a025f4d8a7e07ac3b14488
R0quef0rt <ryanjbrooks11@gmail.com>
2017-05-30 19:46:57 -0500

    Add Run-AzureSync.ps1

---
+++ Office 365/Run-AzureSync.ps1
@@ -0,0 +1,5 @@
+﻿## This script is used to run a scheduled task that syncs the Azure AD Connect tool between DOMAIN.com and Microsoft Azure
+
+$azureserver = "server.domain.com"
+
+Invoke-Command -ComputerName $azureserver {Start-ADSyncSyncCycle -PolicyType Delta}
